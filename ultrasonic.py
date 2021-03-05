5a51e79bc690f3d2bcf8e2058630b214aa09b38c
olenesheim <ole.s.nesheim@gmail.com>
2021-03-05 09:44:51 +0100

    Changes made to us

--- ultrasonic.py
+++ ultrasonic.py
@@ -3,6 +3,11 @@ from time import sleep
 import numpy as np
 
 if __name__ == "__main__":
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(1, GPIO.OUT)
+    GPIO.setup(16, GPIO.IN)
+
     sensor = DistanceSensor(1, 16) #Trig = GPIO1, Echo = GPIO16
     while True:
         print("Distance: ", sensor.distance)
