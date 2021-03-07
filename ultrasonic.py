eab90538b351fae4f8944af34923b46619dc1dce
olenesheim <ole.s.nesheim@gmail.com>
2021-03-07 18:05:57 +0100

    ultrasound ready

--- ultrasonic.py
+++ ultrasonic.py
@@ -7,17 +7,9 @@ if __name__ == "__main__":
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(1, GPIO.OUT)
-
-    GPIO.output(1, GPIO.HIGH)
-    sleep(3)
-    GPIO.output(1, GPIO.LOW)
-
-
     GPIO.setup(16, GPIO.IN)
 
     sensor = DistanceSensor(1, 16) #Trig = GPIO1, Echo = GPIO16
     while True:
         print("Distance: ", sensor.distance)
         sleep(1)
-
-        #end
