6e5cf086ac098098fe641a8ad60c56a05d8456ec
olenesheim <ole.s.nesheim@gmail.com>
2021-03-08 11:30:38 +0100

    added graph-maker-program

--- graph_maker.py
+++ graph_maker.py
@@ -6,11 +6,11 @@ import numpy as np
 def make_array_from_txt(filename):
 
     array = []
-    file1 = open(filename, "a")
+    file1 = open(filename, "r")
 
-    while file1.readline():
-        line = file1.readline()
-        array.append(line)
+    for line in file1:
+        line = line.strip("\n")
+        array.append(float(line))
 
     file1.close()
     return array
@@ -20,11 +20,10 @@ if __name__ == "__main__":
     print("What file do you want to plot?")
     filename = input()
 
-    x = np.linspace(0, 10)
     y = make_array_from_txt(filename)
     
-    plt.plot(x, y)
+    plt.plot(y)
     plt.xlabel("Time")
     plt.ylabel("Distance")
-    plt.title("Distance measured from file - ", filename)
+    plt.title("Distance measured from file - " + filename)
     plt.show()
